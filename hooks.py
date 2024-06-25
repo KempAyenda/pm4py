04fef71a5cdc715dde441ce78cfb26fabd00ea5b
KempAyenda <148476568+KempAyenda@users.noreply.github.com>
2024-06-25 14:42:53 -0400

    Create hooks.py

---
+++ hooks.py
@@ -0,0 +1,10 @@
+
+# hooks.py
+app_name = "pm4py"
+app_title = "pm4py"
+app_publisher = "Eola Organizational Research and Design LLC"
+app_description = "A Frappe app for process analysis"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "ayenda@eolaord.com"
+app_license = "MIT"
